3c7fbdfaf929c4fff1e2f393c96f5bf0233135e4
ShamKarthikS-Hexaware <1000055576@hexaware.com>
2022-11-04 13:57:24 +0000

    java

---
+++ onespring/src/main/java/com/onespring/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.onespring.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	public EntityNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
